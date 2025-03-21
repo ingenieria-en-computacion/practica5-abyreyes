b2893157a828e25b58cf09033a765aff992aebc5
abyreyes <aby.08reyes@gmail.com>
2025-03-20 23:15:59 -0600

    Update queue.c

--- Cola/Cola_arreglos_dinamicos/src/queue.c
+++ Cola/Cola_arreglos_dinamicos/src/queue.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 struct Queue {
-    int *data;   
-    int front;   
-    int rear;    
-    int len;  
+    Data *data;  
+    int head;    
+    int tail;    
+    int len;     
 };
 
 //Crea una nueva cola vacía y la devuelve.
@@ -26,55 +26,55 @@ Queue queue_create(int len){
 } 
 
 //Inserta un elemento al final de la cola.
-void queue_enqueue(Queue* q, int d) {
-    if (q->rear == q->len - 1) {
+void queue_enqueue(Queue* q, Data d) {
+    if (q->tail == q->len - 1) {
         // La cola está llena
         return;
     }
-    if (q->front == -1) {
-        q->front = 0; // Si la cola estaba vacía, se coloca el primer elemento
+    if (q->head == -1) {
+        q->head = 0; 
     }
-    q->rear++;
-    q->data[q->rear] = d; // Insertar el dato en el final de la cola
+    q->tail++;
+    q->data[q->tail] = d; 
 }
 
 //Elimina y devuelve el elemento al frente de la cola.
-Data queue_dequeue(Queue* q){
-  if (q->front == -1) {
+Data queue_dequeue(Queue* q) {
+    if (q->head == -1) {
         // La cola está vacía
         return -1; // Valor de error
     }
-    int data = q->data[q->front];
-    q->front++;
-    if (q->front > q->rear) {
-        // La cola quedó vacía
-        q->front = q->rear = -1;
+    Data d = q->data[q->head];
+    q->head++;
+    if (q->head > q->tail) {
+        // La cola queda vacía
+        q->head = q->tail = -1;
     }
-    return data;
+    return d;
 }
 
 //Verifica si la cola está vacía.
 bool queue_is_empty(Queue* q) {
-    return q->front == -1;
+    return q->head == -1;
 }
 
 //Obtiene el elemento al frente de la cola sin eliminarlo.
-Data queue_front(Queue* q){
-  if (q->front == -1) {
+Data queue_front(Queue* q) {
+    if (q->head == -1) {
         // La cola está vacía
         return -1; // Valor de error
     }
-    return q->data[q->front];
+    return q->data[q->head];
 }
 
 //Vacía la cola, eliminando todos sus elementos
-void queue_empty(Queue* q){
-  q->front = q->rear = -1; 
+void queue_empty(Queue* q) {
+    q->head = q->tail = -1; 
 }
 
 //Elimina la cola y libera la memoria asociada a ella.
-void queue_delete(Queue* q){
-   if (q) {
+void queue_delete(Queue* q) {
+    if (q) {
         free(q->data); 
         free(q);      
     }
