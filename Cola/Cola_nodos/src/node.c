56f3fb5105bd13fdde3a04b5b766bd86140673d6
abyreyes <aby.08reyes@gmail.com>
2025-03-20 23:24:25 -0600

    Update node.c

--- Cola/Cola_nodos/src/node.c
+++ Cola/Cola_nodos/src/node.c
@@ -1,40 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "node.h"
 
-/**
- * Crea un nuevo nodo con los datos proporcionados y lo devuelve.
- * 
- * @param d Dato que se almacenará en el nuevo nodo.
- * @return Un apuntador al nuevo nodo creado. Si la creación falla, devuelve NULL.
- * @details Esta función asigna memoria dinámicamente para un nuevo nodo utilizando `malloc`.
- *          Si la asignación de memoria falla, la función devuelve NULL. El nodo creado
- *          tiene sus campos inicializados, y el campo de datos se establece con el valor
- *          proporcionado en el parámetro `d`, el siguiente .
- */
+//Crea un nuevo nodo con los datos proporcionados y lo devuelve.
 Node *new_node(Data d){
-
+      Node *n = (Node *)malloc(sizeof(Node));
+    if (n == NULL) {
+        return NULL;
+    }
+    n->data = d;
+    n->next = NULL; 
+    return n;
 }
 
-/**
- * Elimina un nodo y libera la memoria asociada a él.
- * 
- * @param n Apuntador al nodo que se desea eliminar.
- * @details Esta función libera la memoria asignada dinámicamente para el nodo utilizando `free`.
- *          Si el apuntador pasado es NULL, la función no realiza ninguna operación.
- *          Es responsabilidad del llamante asegurarse de que el nodo ya no se utiliza después
- *          de ser eliminado. Está función solo libera nodos cuyo enlace al siguiente es nulo
- */
+//Elimina un nodo y libera la memoria asociada a él.
 void delete_node(Node* n){
-
+  if (n == NULL) {
+        return; // No hacer nada si el nodo es NULL
+    }
+    if (n->next == NULL) { // Solo elimina si no tiene siguiente
+        free(n);
+    }
 }
 
-/**
- * Imprime la información contenida en un nodo.
- * 
- * @param n Apuntador al nodo que se desea imprimir.
- * @details Esta función imprime los campos del nodo, como su valor y punteros a otros nodos,
- *          en un formato legible. Si el puntero pasado es NULL, la función imprime un mensaje
- *          indicando que el nodo es inválido. La salida se dirige a la salida estándar (stdout).
- */
+//Imprime la información contenida en un nodo.
 void print_node(Node* n){
-
+  if (n == NULL) {
+        printf("Nodo inválido\n");
+        return;
+    }
+    printf("Nodo: [Data: %d | Next: %p]\n", n->data, (void*)n->next);
 }
